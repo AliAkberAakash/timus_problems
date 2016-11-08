2716d113f65cff863a650fbb1f957d0e4b74ed19
AliAkberAakash <ali852609@gmail.com>
2016-11-08 23:53:58 +0600

    Add files via upload

---
+++ timus 1083.cpp
@@ -0,0 +1,29 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+
+
+int main()
+{
+    using namespace std;
+
+    int k,l,n,i,j,t=1;
+    char s[100];
+
+    scanf("%d", &n);
+    getchar();
+    gets(s);
+
+    k=strlen(s);
+
+    while(n>0)
+    {
+        t=t*n;
+        n=n-k;
+    }
+
+    printf("%d\n", t);
+
+    return 0;
+}
+
