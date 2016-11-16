1ff236537ddeda4c3c92eca33c345df3f668238a
AliAkberAakash <ali852609@gmail.com>
2016-11-16 09:00:17 +0600

    Add files via upload

---
+++ timus 1001.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+double a[128 * 1024];
+
+int main()
+{
+    unsigned long long d;
+    int i=0;
+
+    while(scanf("%llu", &d)!=EOF)
+    {
+        a[i++]=(double) sqrt(d * 1.0);
+    }
+
+    while(--i >=0)
+    {
+        printf("%.4lf\n", a[i]);
+    }
+
+    return 0;
+}
+
